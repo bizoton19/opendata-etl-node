603960e8a2e1bbbd964a7bcd8d3d03794767205d
bizoton19 <bizoton19@gmail.com>
2018-08-20 07:29:41 -0400

    tsconfig

---
+++ dist/src/tests/abstractModelFactoryTest.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const abstractModelFactory_1 = require("../factories/abstractModelFactory");
+class abstractModelFactoryTest extends abstractModelFactory_1.abstractModelFactory {
+    CreateModel(arg) {
+        throw new Error("Method not implemented.");
+    }
+}
--- src/factories/abstractModelFactory.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var abstractModelFactory = /** @class */ (function () {
-    function abstractModelFactory() {
-    }
-    return abstractModelFactory;
-}());
-exports.abstractModelFactory = abstractModelFactory;
