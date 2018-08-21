7f216dbde2e0d3303c981ace0e094c87d274cde3
bizoton19 <bizoton19@gmail.com>
2018-08-21 07:49:52 -0400

    added test

--- src/tests/abstractModelFactoryTest.ts
+++ src/tests/abstractModelFactoryTest.ts
@@ -1,9 +1,30 @@
 import {  Artifact} from "../model/Artifact";
 import {  abstractModelFactory}  from "../factories/abstractModelFactory";
 
+
+class Recall implements Artifact {
+    uuid!: string;    
+    title!: string;
+    type!: string;
+    artifactDate!: Date;
+    artifactSiource!: string;
+    description!: string;
+    category!: string;
+    fullTextSearch!: string;
+
+   
+}
 class abstractModelFactoryTest extends abstractModelFactory {
     CreateModel<T extends Artifact>(arg: T): T {
-        throw new Error("Method not implemented.");
+        return arg;
     }
    
 }
+
+let t = new Recall();
+t.title = "new hazard"
+t.uuid = "887jjjj"
+let test = new abstractModelFactoryTest()
+let createdModel = test.CreateModel(t)
+createdModel.title
+
